073e1a8d1584d619f85a925316da6b4434c3d0d0
yfu03 <yannisfu@gmail.com>
2025-02-23 17:55:21 -0600

    basic, bracket, projection operator overloads done

--- analyzer/lib/Vector3D.cpp
+++ analyzer/lib/Vector3D.cpp
@@ -2,14 +2,22 @@
 
 using namespace arro;
 
-Vector3D& Vector3D::operator=(const Vector3D& other) {
+Vector3D& Vector3D::operator=(const Vector3D& other) 
+{
   _x = other._x;
 	_y = other._y;
 	_z = other._z;
+  
   return *this;
 }
 
-Vector3D& Vector3D::operator+=(const Vector3D& other) {
+Vector3D operator+(const Vector3D& a,  const Vector3D& b)
+{
+  return Vector3D(a._x + b._x, a._y + b._y, a._z + b._z);
+}
+
+Vector3D& Vector3D::operator+=(const Vector3D& other) 
+{
 	_x += other._x;
 	_y += other._y;
 	_z += other._z;
@@ -17,7 +25,13 @@ Vector3D& Vector3D::operator+=(const Vector3D& other) {
 	return *this;
 }
 
-Vector3D& Vector3D::operator-=(const Vector3D& other) {
+Vector3D operator-(const Vector3D& a,  const Vector3D& b)
+{
+  return Vector3D(a._x - b._x, a._y - b._y, a._z - b._z);
+}
+
+Vector3D& Vector3D::operator-=(const Vector3D& other) 
+{
 	_x -= other._x;
 	_y -= other._y;
 	_z -= other._z;
@@ -25,16 +39,55 @@ Vector3D& Vector3D::operator-=(const Vector3D& other) {
 	return *this;
 }
 
-Vector3D& Vector3D::operator*=(double scale) {
+Vector3D operator*(const Vector3D& vec, double scale) 
+{
+	return Vector3D(vec._x * scale, vec._y * scale, vec._z * scale);
+}
+
+double operator*(const Vector3D& a, const Vector3D& b)
+{
+  return a._x * b._x + a._y * b._y + a._z * b._z;
+}
+
+Vector3D& Vector3D::operator*=(double scale) 
+{
   _x *= scale;
 	_y *= scale;
 	_z *= scale;
+
   return *this;
 }
 
-Vector3D& Vector3D::operator/=(double scale) {
+Vector3D operator/(const Vector3D& vec, double scale) 
+{
+	return Vector3D(vec._x / scale, vec._y / scale, vec._z / scale);
+}
+
+Vector3D& Vector3D::operator/=(double scale) 
+{
   _x /= scale;
 	_y /= scale;
 	_z /= scale;
+
   return *this;
+}
+
+Vector3D operator%(const Vector3D& a, const Vector3D& b)
+{
+  return b()  * (a * b)  / b.magnitude();
+}
+
+Vector3D& Vector3D::operator%=(const Vector3D& other) 
+{
+	return (*this = *this % other);
+}
+
+Vector3D Vector3D::operator()() const 
+{
+	return *this / magnitude();
+}
+
+double Vector3D::operator[](const Vector3D&  other) const
+{
+  return (*this * other) / other.magnitude();
 }
