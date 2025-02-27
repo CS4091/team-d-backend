d7f920f4eda783ff49a7d4e8e345bf72474204ad
Jason Xu <Xinwen31415@gmail.com>
2025-02-27 13:34:32 -0600

    added some more aviation calculations and fixed naming of descent pitch

--- analyzer/lib/math/aviation.cpp
+++ analyzer/lib/math/aviation.cpp
@@ -8,17 +8,55 @@ using namespace arro;
 
 const double aviation::CRUISING_ALTITUDE = 30'000 * 0.3048;		// flight level 3000
 const double aviation::TAKEOFF_PITCH = 10 * numbers::pi / 180;	// 10deg takeoff climb
-const double aviation::LANDING_PITCH = -3 * numbers::pi / 180;	// 3deg final approach
+const double aviation::DESCENT_PITCH = -3 * numbers::pi / 180;	// 3deg final approach
 
 // lateral distance covered on a climb to flight level 3000 (also stdlib doesn't have cotangent for some reason??)
 const double aviation::CLIMB_LATERAL = CRUISING_ALTITUDE * (1 / tan(TAKEOFF_PITCH));
 const double aviation::CLIMB_DISTANCE = CRUISING_ALTITUDE * (1 / sin(TAKEOFF_PITCH));
 // lateral distance covered on a descent from flight level 3000
-const double aviation::DESCENT_LATERAL = -CRUISING_ALTITUDE * (1 / tan(LANDING_PITCH));
-const double aviation::DESCENT_DISTANCE = -CRUISING_ALTITUDE * (1 / sin(LANDING_PITCH));
+const double aviation::DESCENT_LATERAL = -CRUISING_ALTITUDE * (1 / tan(DESCENT_PITCH));
+const double aviation::DESCENT_DISTANCE = -CRUISING_ALTITUDE * (1 / sin(DESCENT_PITCH));
 
 // lateral distance covered assuming a full climb to flight level 3000 followed by descent
 const double aviation::FULL_CLIMB_LATERAL = CLIMB_LATERAL + DESCENT_LATERAL;
+
+double aviation::climbDistance(const Vector3D& from, const Vector3D& to) {
+	double lateralDistance = (to - from).magnitude();
+
+	if (lateralDistance < FULL_CLIMB_LATERAL) {
+		// a/sin(A) = b/sin(B) = c/sin(C)
+		double lateralRatio = lateralDistance / sin((180 - 10 - 3) * numbers::pi / 180);
+
+		return lateralRatio * sin(TAKEOFF_PITCH);
+	} else {
+		return CLIMB_DISTANCE;
+	}
+}
+
+double aviation::descentDistance(const Vector3D& from, const Vector3D& to) {
+	double lateralDistance = (to - from).magnitude();
+
+	if (lateralDistance < FULL_CLIMB_LATERAL) {
+		// a/sin(A) = b/sin(B) = c/sin(C)
+		double lateralRatio = lateralDistance / sin((180 - 10 - 3) * numbers::pi / 180);
+
+		return lateralRatio * sin(-DESCENT_PITCH);
+	} else {
+		return DESCENT_DISTANCE;
+	}
+}
+
+double aviation::cruiseDistance(const Vector3D& from, const Vector3D& to) {
+	double lateralDistance = (to - from).magnitude();
+
+	double lateralDistance = (to - from).magnitude();
+
+	if (lateralDistance < FULL_CLIMB_LATERAL) {
+		return 0;
+	} else {
+		return lateralDistance - CLIMB_LATERAL - DESCENT_LATERAL;
+	}
+}
 
 double aviation::flightDistance(const Vector3D& from, const Vector3D& to) {
 	double lateralDistance = (to - from).magnitude();
@@ -27,7 +65,7 @@ double aviation::flightDistance(const Vector3D& from, const Vector3D& to) {
 		// a/sin(A) = b/sin(B) = c/sin(C)
 		double lateralRatio = lateralDistance / sin((180 - 10 - 3) * numbers::pi / 180);
 
-		double climbDistance = lateralRatio * sin(TAKEOFF_PITCH), descentDistance = lateralRatio * sin(LANDING_PITCH);
+		double climbDistance = lateralRatio * sin(TAKEOFF_PITCH), descentDistance = lateralRatio * sin(-DESCENT_PITCH);
 
 		return climbDistance + descentDistance;
 	} else {
--- analyzer/lib/math/aviation.h
+++ analyzer/lib/math/aviation.h
@@ -7,12 +7,18 @@ namespace arro {
 namespace aviation {
 extern const double CRUISING_ALTITUDE;
 extern const double TAKEOFF_PITCH;
-extern const double LANDING_PITCH;
+extern const double DESCENT_PITCH;
 extern const double CLIMB_LATERAL;
 extern const double CLIMB_DISTANCE;
 extern const double DESCENT_LATERAL;
 extern const double DESCENT_DISTANCE;
 extern const double FULL_CLIMB_LATERAL;
+
+double climbDistance(const Vector3D& from, const Vector3D& to);
+
+double descentDistance(const Vector3D& from, const Vector3D& to);
+
+double cruiseDistance(const Vector3D& from, const Vector3D& to);
 
 double flightDistance(const Vector3D& from, const Vector3D& to);
 }  // namespace aviation
