d8e4d1d811b56988f4d2062ec57e41a589a5d2aa
Jason Xu <Xinwen31415@gmail.com>
2025-03-30 23:57:13 -0500

    oops lmao

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DBModule } from 'src/db/db.module';
+import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
 	imports: [DBModule],
+	controllers: [UsersController],
 	providers: [UsersService],
 	exports: [UsersService]
 })
